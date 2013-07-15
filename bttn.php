d500d865717b35647bc36a72f018dbcd3a3e0e47
Dan Shevenell <business@edentification.com>
2013-07-15 00:28:23 -0500

    refactor bttn

--- bttn.php
+++ bttn.php
@@ -24,8 +24,9 @@
 		$tb_em = '0';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<script src="https://api.oneid.com/js/includeexternal.js" type="text/javascript"></script>
+<script src="https://api.oneid.com/form/form.js" type="text/javascript"></script>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -35,85 +36,79 @@
 
 <!-- CSS for the button !-->
 <style type="text/css">
-.tinybttn {
-	-moz-box-shadow:inset 0px 1px 0px 0px #fce2c1;
-	-webkit-box-shadow:inset 0px 1px 0px 0px #fce2c1;
-	box-shadow:inset 0px 1px 0px 0px #fce2c1;
-	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #ffc477), color-stop(1, #fb9e25) );
-	background:-moz-linear-gradient( center top, #ffc477 5%, #fb9e25 100% );
-	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffc477', endColorstr='#fb9e25');
-	background-color:#ffc477;
-	-moz-border-radius:6px;
-	-webkit-border-radius:6px;
-	border-radius:6px;
-	border:1px solid #eeb44f;
-	display:inline-block;
-	color:#ffffff;
-	font-family:arial;
-	font-size:15px;
-	font-weight:normal;
-	padding:7px 12px;
-	text-decoration:none;
-	text-shadow:1px 1px 0px #cc9f52;
-}.tinybttn:hover {
-	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #fb9e25), color-stop(1, #ffc477) );
-	background:-moz-linear-gradient( center top, #fb9e25 5%, #ffc477 100% );
-	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#fb9e25', endColorstr='#ffc477');
-	background-color:#fb9e25;
-}.tinybttn:active {
-	position:relative;
-	top:1px;
-}
+	.tinybttn {
+		-moz-box-shadow:inset 0px 1px 0px 0px #fce2c1;
+		-webkit-box-shadow:inset 0px 1px 0px 0px #fce2c1;
+		box-shadow:inset 0px 1px 0px 0px #fce2c1;
+		background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #ffc477), color-stop(1, #fb9e25) );
+		background:-moz-linear-gradient( center top, #ffc477 5%, #fb9e25 100% );
+		filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffc477', endColorstr='#fb9e25');
+		background-color:#ffc477;
+		-moz-border-radius:6px;
+		-webkit-border-radius:6px;
+		border-radius:6px;
+		border:1px solid #eeb44f;
+		display:inline-block;
+		color:#ffffff;
+		font-family:arial;
+		font-size:15px;
+		font-weight:normal;
+		padding:7px 12px;
+		text-decoration:none;
+		text-shadow:1px 1px 0px #cc9f52;
+	}.tinybttn:hover {
+		background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #fb9e25), color-stop(1, #ffc477) );
+		background:-moz-linear-gradient( center top, #fb9e25 5%, #ffc477 100% );
+		filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#fb9e25', endColorstr='#ffc477');
+		background-color:#fb9e25;
+	}.tinybttn:active {
+		position:relative;
+		top:1px;
+	}
 </style>
+
+
+
 <body style="font-family: arial; font-size: small;">
+	
 
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-<script src="https://api.oneid.com/js/includeexternal.js" type="text/javascript"></script>
-<script src="https://api.oneid.com/form/form.js" type="text/javascript"></script>
-
-
-<!-- The javascript to determine if OneID needs to be called to get the user's credentials !-->
-<script type="text/javascript">
-	function getTinyBttnDiscounts(){
-		if(<?php echo $tb_id; ?>){
-			$.post("bttn_result.php", { 1 : '<?php echo $_SESSION['tinybttn_email']; ?>', 2 : '<?php echo $_SESSION['tinybttn_id']; ?>' } )
-		}
+	<script type="text/javascript">
+		function getTinyBttnDiscounts(){
+			if(<?php echo $tb_id; ?>){
+				$.post("bttn_result.php", { 1 : '<?php echo $_SESSION['tinybttn_email']; ?>', 2 : '<?php echo $_SESSION['tinybttn_id']; ?>' } )
+			}
 		
-		else if(<?php echo $tb_em; ?>){
-			$.post("bttn_result.php", { 1 : '<?php echo $_SESSION['tinybttn_email']; ?>' } )
-		}
+			else if(<?php echo $tb_em; ?>){
+				$.post("bttn_result.php", { 1 : '<?php echo $_SESSION['tinybttn_email']; ?>' } )
+			}
 
-		else{
-				OneIdExtern.registerApiReadyFunction(
-					function(){
-						OneId.getUserAttributes(
-							{
-								attr: "email[email] TinyBttn[ID]",
-								authLevel:null,
-								selectCards:false,
-								forceSelectCards:false
-							},
-							function(data){
-								if(data.attribute_data.TinyBttn){
-									$.post("bttn_result.php", { 1 : data.attribute_data.email.email, 2 : data.attribute_data.TinyBttn.ID } )
+			else{
+					OneIdExtern.registerApiReadyFunction(
+						function(){
+							OneId.getUserAttributes(
+								{
+									attr: "email[email] TinyBttn[ID]",
+									authLevel:null,
+									selectCards:false,
+									forceSelectCards:false
+								},
+								function(data){
+									if(data.attribute_data.TinyBttn){
+										$.post("bttn_result.php", { 1 : data.attribute_data.email.email, 2 : data.attribute_data.TinyBttn.ID } )
+									}
+									else {
+										$.post("bttn_result.php", { 1 : data.attribute_data.email.email } )
+									}
 								}
-								else {
-									$.post("bttn_result.php", { 1 : data.attribute_data.email.email } )
-								}
-							}
-						);
-					}
-				);
-		}
-	};
+							);
+						}
+					);
+			}
+		};
 
-</script>
-	<div id="wrapper">
-		<button class="tinybttn" onclick="getTinyBttnDiscounts()">TinyBttn</button>
-		<span class="slogan"></span>
-		<p>
-	</div>
-    <div id="response"></div>
+	</script>
+
+	<button class="tinybttn" onclick="getTinyBttnDiscounts()">TinyBttn</button>
+
 </body>
 </html>
